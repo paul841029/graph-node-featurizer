457cffc1bf257e2e9e008ddc96955465da9c9b7b
Paul Luh <paul841029@gmail.com>
2019-11-17 17:02:13 +0000

    more files

--- experiment.sh
+++ experiment.sh
@@ -1,14 +1,26 @@
-for DATASET in political_table president_mix transistor
-do
-    for ESIZE in 1 3 5 10 15 20
-    do
-        make example=${ESIZE} data=${DATASET} gt_level=cell snuba_exp
-        make example=${ESIZE} data=${DATASET} gt_level=cell snuba_exp
-        make example=${ESIZE} data=${DATASET} gt_level=cell snuba_exp
-        make example=${ESIZE} data=${DATASET} gt_level=cell snuba_exp
-        make example=${ESIZE} data=${DATASET} gt_level=cell snuba_exp 
-    done
-done
+# for DATASET in political_table president_mix transistor
+# do
+#     for ESIZE in 1 3 5 10 15 20
+#     do
+#         make example=${ESIZE} data=${DATASET} gt_level=cell snuba_exp
+#         make example=${ESIZE} data=${DATASET} gt_level=cell snuba_exp
+#         make example=${ESIZE} data=${DATASET} gt_level=cell snuba_exp
+#         make example=${ESIZE} data=${DATASET} gt_level=cell snuba_exp
+#         make example=${ESIZE} data=${DATASET} gt_level=cell snuba_exp 
+#     done
+# done
+
+# for DATASET in cdr
+# do
+#     for ESIZE in 1 3 5 10 15 20 30 40 60 70
+#     do
+#         make example=${ESIZE} data=${DATASET} gt_level=span snuba_exp
+#         make example=${ESIZE} data=${DATASET} gt_level=span snuba_exp
+#         make example=${ESIZE} data=${DATASET} gt_level=span snuba_exp
+#         make example=${ESIZE} data=${DATASET} gt_level=span snuba_exp
+#         make example=${ESIZE} data=${DATASET} gt_level=span snuba_exp 
+#     done
+# done
 
 for DATASET in transistor president_mix
 do
@@ -22,14 +34,3 @@ do
     done
 done
 
-for DATASET in cdr
-do
-    for ESIZE in 1 3 5 10 15 20 30 40 60 70
-    do
-        make example=${ESIZE} data=${DATASET} gt_level=span snuba_exp
-        make example=${ESIZE} data=${DATASET} gt_level=span snuba_exp
-        make example=${ESIZE} data=${DATASET} gt_level=span snuba_exp
-        make example=${ESIZE} data=${DATASET} gt_level=span snuba_exp
-        make example=${ESIZE} data=${DATASET} gt_level=span snuba_exp 
-    done
-done
